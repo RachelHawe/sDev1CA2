92f6172bd4dc27e8a1e99f67fcf6aa56be11b73e
Lara Chiappetta Lagioia <laraclagioia@gmail.com>
2016-11-02 10:55:31 +0000

    Add files via upload

---
+++ LaraLagioia_RachelHawe.java
@@ -0,0 +1,17 @@
+/*
+ * This program...
+ * 25/11/2016
+ * Lara Lagioia - X00141126 & Rachel Hawe - X00127602
+ */
+package ca2;
+
+import java.util.Scanner;
+
+public class LaraLagioia_RachelHawe {
+
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+        
+    }
+    
+}
